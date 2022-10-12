49872ecb74ca17f33f291ce7b6e9e2d71d533801
Pasquale Aragosa <aragosap@gmail.com>
2022-10-12 16:07:07 +0200

    add append

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,26 @@
+//Aggancio il div tramite la sua classe "images" nel DOM
 const imagesEl = document.querySelector('.images');
 
+//Creo un array di immagini
 const slides = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg',
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp',
 ];
+
+
+imagesEl.append(slides[0]);
+
+
+
+//Scorro tutte le immagini
+// for (let i = 0; i < slides.length; i++) {
+//     const slideEl = slides[i];
+//     const slideMarkup = `<img class="active" src=>`;
+    
+// }
 
 
 
