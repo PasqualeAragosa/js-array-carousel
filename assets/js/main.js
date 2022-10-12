3068f52556833163a9c7b4c485b09998ef48dae1
Pasquale Aragosa <aragosap@gmail.com>
2022-10-12 15:17:32 +0200

    add images in style.css

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+//Aggancio il container
+const containerEl = document.querySelector('.container');
+//Creo un div
+const divEl = document.createElement('div');
+//Inserisco il div nel container
+containerEl.append(divEl);
+//Aggiungo al div la classe 'images'
+divEl.classList.add('images');
+//Aggancio il div tramite la sua classe
+let imagesEl = document.querySelector('.images');
+
+
+
+
+
+
