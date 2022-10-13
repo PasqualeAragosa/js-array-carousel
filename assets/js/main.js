1353327a60cc819f144286989d4f10209d9d082e
Pasquale Aragosa <aragosap@gmail.com>
2022-10-13 12:20:02 +0200

    add loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,66 @@
-//Seleziono il carosello
-const carouselEl = document.querySelector('.carousel');
-
-//Array di immagini
-const imagesList = [
-    "assets/img/01.webp",
-    "assets/img/02.webp",
-    "assets/img/03.webp",
-    "assets/img/04.webp",
-    "assets/img/05.webp",
+const slides = [
+    './assets/img/01.webp',
+    './assets/img/02.webp',
+    './assets/img/03.webp',
+    './assets/img/04.webp',
+    './assets/img/05.webp'
 ];
 
-//Creo una variabile per tenere traccia dell'immagine corrente
-let imageCurr = 0;
+//Seleziono elementi della DOM
+const slidesElement = document.querySelector('.slides');
+const nextEl = document.querySelector('.next');
+const prevEl = document.querySelector('.prev');
 
-//Scorro tutte le immagini
-for (let i = 0; i < imagesList.length; i++) {
-    const  imageNow = imagesList[i];
-    const imageMark = `<img class="${i === imageCurr ? 'active' : ''}" src="${imageNow}" alt="">`;
-    carouselEl.insertAdjacentHTML("afterbegin", imageMark);
+//Seleziono l'indice dell'immagine 
+let activeImg = 0;
+
+//Inserisco dinamicamente le immagini
+for (let i = 0; i < slides.length; i++) {
+    const slideUrl = slides[i];
+    const imgMarkup = `<img src="${slideUrl}" alt="" class="img-fluid ${i === activeImg ? 'active' : ''}">`;
+    slidesElement.insertAdjacentHTML('beforeend', imgMarkup); 
 }
+
+let nextSlide;
+
+//Aggiungo event Listener alla freccia next
+nextEl.addEventListener('click', function() {
+    //Seleziono l'immagine attiva
+    const activeSlideEl = document.querySelector('.slides > img.active');
+    //Rimuovo la classe active dall'immagine attiva
+    activeSlideEl.classList.remove('active');
+    //Inremento l'indice
+    activeImg++;
+    console.log('activeImg: ', activeImg);
+    //Creo un array selezionando tutte le immagini
+    const allSlides = document.getElementsByClassName('img-fluid');
+    //
+    if (activeImg === 5) {
+        activeImg = 0;
+    } else {
+        //Seleziono l'immagine successiva
+        nextSlide = allSlides[activeImg];
+    }
+    //Gli aggiungo la classe active
+    nextSlide.classList.add('active');
+
+});
+
+let prevSlide;
+
+//Aggiungo event Listener alla freccia prev
+prevEl.addEventListener('click', function() {
+    const activeSlideEl = document.querySelector('.slides > img.active');
+    activeSlideEl.classList.remove('active');
+    activeImg--;
+    console.log('activeImg: ', activeImg);
+    const allSlides = document.getElementsByClassName('img-fluid');
+    if (activeImg === -1) {
+        activeImg = 4;
+        console.log('if - activeImg: ', activeImg);
+    } else {
+        prevSlide = allSlides[activeImg];
+    }
+    prevSlide = allSlides[activeImg];
+    prevSlide.classList.add('active');
+});
